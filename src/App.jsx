a9c1710f807440e4961bf6d265b4801c12be22be
Chinmay <chinmaybagad4@gmail.com>
2024-07-07 07:37:26 +0530

    Completed Analytics Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Analytics from "./components/Analytics";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
@@ -6,6 +7,7 @@ export default function App() {
     <div>
       <NavBar />
       <Hero />
+      <Analytics />
     </div>
   );
 }
