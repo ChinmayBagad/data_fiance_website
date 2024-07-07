38fc3b2417b79294c46f82036d828d8995ae9789
Chinmay <chinmaybagad4@gmail.com>
2024-07-08 04:47:47 +0530

    Completed Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Newsletter from "./components/Newsletter";
 import Card from "./components/Card";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Analytics />
       <Newsletter />
       <Card />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,73 @@
+import {
+  FaDribbble,
+  FaFacebook,
+  FaGithub,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 grid-cols-1 gap-8 text-gray-300">
+      <div>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+        <p className="py-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          repellendus alias, cum blanditiis architecto provident deserunt quam
+          ducimus excepturi. Quibusdam aspernatur molestias deserunt quae
+          laboriosam earum vitae iure inventore dolore.
+        </p>
+        <div className="flex gap-3 cursor-pointer ">
+          <FaFacebook
+            size={30}
+            className="hover:bg-blue-600 border-none rounded p-1"
+          />
+          <FaDribbble size={30} className="hover:bg-[#ea4c89] rounded p-1" />
+          <FaInstagram size={30} className="hover:bg-[#5b51d8] rounded p-1" />
+          <FaGithub size={30} className="hover:bg-[#2b3137] rounded p-1" />
+          <FaTwitter size={30} className="hover:bg-[#1DA1F2] rounded p-1" />
+        </div>
+      </div>
+      <div className="lg:col-span-2 justify-between flex mt-6">
+        <div>
+          <h6 className="font-medium text-gray-400">Solutions</h6>
+          <ul>
+            <li className="py-2 text-sm">Analytics</li>
+            <li className="py-2 text-sm">Marketing</li>
+            <li className="py-2 text-sm">Commerce</li>
+            <li className="py-2 text-sm">Insights</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Support</h6>
+          <ul>
+            <li className="py-2 text-sm">Pricing</li>
+            <li className="py-2 text-sm">Documentation</li>
+            <li className="py-2 text-sm">Guides</li>
+            <li className="py-2 text-sm">API Status</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Company</h6>
+          <ul>
+            <li className="py-2 text-sm">About</li>
+            <li className="py-2 text-sm">Blog</li>
+            <li className="py-2 text-sm">Jobs</li>
+            <li className="py-2 text-sm">Press</li>
+            <li className="py-2 text-sm">Careers</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Legal</h6>
+          <ul>
+            <li className="py-2 text-sm">Claim</li>
+            <li className="py-2 text-sm">Policy</li>
+            <li className="py-2 text-sm">Terms</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
